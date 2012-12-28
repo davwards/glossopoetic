85c471823419fff23bc3e22bc2339a809e67090f
David Edwards <david.edwr@gmail.com>
2012-12-27 20:29:28 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glossopoetic::Application.config.secret_token = '345dfe600dfc7d8121272cea28a4ca9cd110af52ebe962ec4c4ed17fb92c848e9b681ac68e8fa93a51bc3403b0b6dedf147adc52b435e634df6c3ca0461fe1c3'
